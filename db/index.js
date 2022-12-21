1e30eaf1e8d057ce36dc71f77952734346fda059
Kevin Weber <weberkm5@gmail.com>
2022-12-21 12:37:08 -0600

    updated create user and update user

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const {Client} = require('pg');
 const client = new Client('postgres://localhost:5432/juicebox-dev2');
 
-// this function gets all users from the users table 
+// this function gets all users from the users table in the DB
 const getAllUsers = async () => {
     try {
         const {rows} = await client.query(`
@@ -12,20 +12,28 @@ const getAllUsers = async () => {
         return rows;
     } catch (error) {
         console.error('ERROR in getAllUsers');
+        throw error
     }
 };
 
-// this function will create new user in the users table 
-const createUser = async ({username, password}) => {
+
+// this function will create new user in the users table. we destructure username and password because we know that an object will be passed as a parameter wiht those values on it. we pull them off to use as parameters, to then pass as values into the dependency array, to then pass into the SQL query based on position in the array
+// that SQL query then inserts that info in the users table in the respective username and password columns 
+const createUser = async ({
+    username, 
+    password, 
+    name, 
+    location
+}) => {
     try {
         const {rows: [newUser]} = await client.query(`
-        INSERT INTO users(username, password)
-        VALUES ($1, $2)
+        INSERT INTO users(username, password, name, location)
+        VALUES ($1, $2, $3, $4)
         ON CONFLICT (username) DO NOTHING
         RETURNING *
-        `, [username, password]);
+        `, [username, password, name, location]);
 
-        console.log('this is newUser: ', newUser);
+        // console.log('this is newUser: ', newUser);
         return newUser;
     } catch (error) {
         console.error('ERROR in createUser')
@@ -33,8 +41,52 @@ const createUser = async ({username, password}) => {
     }
 };
 
+
+
+// create a function to allow users to update their information (name, location, password, etc)
+
+// try {
+//     const {rows: updatedUser} = await client.query(`
+//     UPDATE users
+//     SET name = $1;
+//        *** Need a string that says 'key = $1', 'key = $2', etc
+//     `, [newname]);
+
+const updateUser = async (id, fields = {}) => {
+    const keys = Object.keys(fields);
+    // ^^ this returns an array of the object (aka fields) key names
+    // in this case, ['name', 'location'] which was passed in the object as paramter in updateUser in seed.js
+    // console.log('this is keys ', keys);
+    
+    const setString = keys.map((key, index) => `${key} = $${index + 1}`);
+    // ^^ here we map over the keys array, pull out each element (key name) and index as we go through. each time, we then create a string that makes the SET info for SQL, that *element/keyname = the position in dependency array which is index + 1* because index starts at 0, but SQL starts at 1 so need those to match
+    // console.log(setString, 'this is setString');
+
+    // console.log(Object.values(fields));
+    // ^^ this returns array of the object (aka fields) value names
+    // in this case, ['Kevin', 'River West'] which was passed in the object as parameter in updateUser in seed.js
+    try {
+        const {rows: [updatedUser]} = await client.query(`
+        UPDATE users
+        SET ${setString}
+        WHERE id=${id}
+        RETURNING *;
+        `, Object.values(fields));
+
+        // console.log('this is updated user', updatedUser);
+        return updatedUser;
+
+    } catch (error) {
+        console.error('ERROR in updateUser');
+        throw error
+    }
+};
+
+
+
 module.exports = {
     client,
     getAllUsers, 
-    createUser
+    createUser, 
+    updateUser
 };
--- db/seed.js
+++ db/seed.js
@@ -1,7 +1,8 @@
 const {
     client,
     getAllUsers, 
-    createUser
+    createUser,
+    updateUser
 } = require('./index');
 
 
@@ -21,7 +22,10 @@ const createTables = async () => {
         CREATE TABLE users (
             id SERIAL PRIMARY KEY, 
             username VARCHAR(255) UNIQUE NOT NULL,
-            password VARCHAR(255) NOT NULL 
+            password VARCHAR(255) NOT NULL, 
+            name VARCHAR(255) NOT NULL, 
+            location VARCHAR(255) NOT NULL, 
+            active BOOLEAN DEFAULT true
         );
         `);
     } catch (error) {
@@ -29,18 +33,34 @@ const createTables = async () => {
     }
 };
 
+// here we use the createUser function in index.js to create a batch of new users to seed our DB 
 const createInitialUsers = async () => {
     try {
         console.log('Beginning to create initial users...');
 
-        const albert = await createUser({username: 'albert', password: 'bertie99'});
-        console.log('this is albert =', albert);
+        const albert = await createUser({
+            username: 'albert', 
+            password: 'bertie99',
+            name: 'Albert Gray',
+            location: 'New York'
+        });
+        // console.log('this is albert =', albert);
 
-        const sandra = await createUser({username: 'sandra', password: '2sandy4me'});
-        console.log('this is sandra =', sandra);
+        const sandra = await createUser({
+            username: 'sandra', 
+            password: '2sandy4me',
+            name: 'Sandra Jones',
+            location: 'Chicago'
+        });
+        // console.log('this is sandra =', sandra);
 
-        const glamgal = await createUser({username: 'glamgal', password: 'glam4life'});
-        console.log('this is glamgal =', glamgal);
+        const glamgal = await createUser({
+            username: 'glamgal', 
+            password: 'glam4life',
+            name: 'Sarah Smith',
+            location: 'Toronto'
+        });
+        // console.log('this is glamgal =', glamgal);
 
         console.log('Finished creating initial users')
     } catch (error) {
@@ -49,7 +69,7 @@ const createInitialUsers = async () => {
     }
 };
 
-
+// here we reuibld our DB
 const rebuildDB = async () => {
 try {
     client.connect();
@@ -57,19 +77,31 @@ try {
     await dropTables();
     await createTables();
     await createInitialUsers();
+    // await updateUser(1, {name: 'Kevin', location: 'River West'});
 } catch (error) {
-    console.error('ERROR in rebuildDB')
+    console.error('ERROR in rebuildDB');
+    throw error;
 }
 }
+
 
 const testDB = async () => {
     try {
         console.log('Beginning to test the database...')
         
         
-
+        console.log('calling allUsers...')
         const allUsers = await getAllUsers();
         console.log('this is allUsers = ', allUsers);
+
+        // allUsers returns array of all users, this test takes the id off the first user in teh array (albert in this case), passes it as a parameter and also an object with the values we want to update to the update user funciton in index.js
+        console.log('calling updateUser on allUsers[0]');
+        const updateUserResult = await updateUser(allUsers[0].id, {
+            name: 'Kevin', 
+            location: 'River West'
+        });
+        
+        console.log('result of update user = ', updateUserResult);
 
         console.log('Finished testing the database!')
     } catch (error) {
